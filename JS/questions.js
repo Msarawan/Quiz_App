d2f7706d798579eadd90edae246acf0876bc2b33
Marjina Sarawan <msarawan@redhat.com>
2022-09-14 23:21:58 +0530

    Quiz App

---
+++ JS/questions.js
@@ -0,0 +1,59 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Which of the following is correct about features of JavaScript?",
+    answer: "All of the above.",
+    options: [
+      "JavaScript is a lightweight, interpreted programming language",
+      "JavaScript is designed for creating network-centric applications",
+      "JavaScript is complementary to and integrated with Java.",
+      "All of the above."
+    ]
+  },
+  {
+    numb: 2,
+    question: "What is the most used programming language in 2019?",
+    answer: "Javascript",
+    options: [
+      "java",
+      "C",
+      "Python",
+      "Javascript"
+    ]
+  },
+  {
+    numb: 3,
+    question: "Which year was JavaScript launched?",
+    answer: 1995,
+    options: [
+      1996,
+      1995,
+      1994,
+      "None of the above"
+    ]
+  },
+
+{
+    numb: 4,
+    question: "What does HTML stand for?",
+    answer:"Hypertext Markup Language",
+    options: [
+        "Hypertext Markup Language",
+        "Cascading Style Sheet",
+        "Jason Object Notation",
+        "Helicopters Terminals Motorboats Lamborginis",
+    ]
+  },
+  {
+    numb: 5,
+    question: "Javascript is an _______ language?",
+    answer: "Object-oriented",
+    options: [
+        "Object-oriented",
+        "Object-based",
+        "Procedural",
+        "None of the above",
+    ]
+  },
+]
+
